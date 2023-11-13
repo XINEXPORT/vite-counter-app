078c5b566ec2a2397f4439d52fedafce2e5dc033
Christine Hoang <christinehoangt@gmail.com>
2023-11-13 14:05:24 -0600

    save

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import setupCounter from './src/counter.js';
+// import "./styles.css";
+
+console.log('Hello from main.js');
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  <h1>Counter App</h1>
 
-setupCounter(document.querySelector('#counter'))
+  <p>Click the button to increase the counter.</p>
+
+  <div id="counter"></div>`;
+
+  setupCounter(document.querySelector('#counter'));
